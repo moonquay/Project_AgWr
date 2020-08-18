83b8940253fe6f75fc345d1cab420b83dd183762
moonquay <growyourownfood@gmail.com>
2020-08-17 19:05:57 -0600

    Added cookie cutter file structure.

---
+++ Project_AgWr/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Predicting agricultural yields based on weather data.',
+    author='Christopher DeVoe',
+    license='MIT',
+)
